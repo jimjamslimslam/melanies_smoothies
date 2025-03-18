147b5ba650527d1fe11e1d3a36f91e2f82d82219
jimjamslimslam <jimjamslimslam@yahoo.com>
2025-03-18 11:48:39 -0400

    Update streamlit_app.py
    Removing display of all API call

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,9 +21,6 @@ pd_df=my_dataframe.to_pandas()
 
 ingredients_list = st.multiselect('Choose up to 5 ingredients:', my_dataframe, max_selections = 5)
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/all")
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
-
 if ingredients_list:
 
     ingredients_string = ''
