c53f654b1e2f6123285f919dbc5a7c7cd8d75260
jimjamslimslam <jimjamslimslam@yahoo.com>
2025-03-18 11:23:49 -0400

    Update streamlit_app.py
    Adding pandas library

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
+import pandas as pd
 
 st.title("Custom Smoothie Order Form :balloon:")
 st.write(f"""Choose the fruits you want in your custom Smoothie!""")
@@ -12,8 +13,9 @@ st.write('The name on your Smoothie will be: ', name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
 ingredients_list = st.multiselect('Choose up to 5 ingredients:', my_dataframe, max_selections = 5)
 
